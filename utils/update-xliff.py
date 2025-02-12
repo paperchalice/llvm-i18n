a2dbf02f5d011888f313fa55b7ba9bf1a5b209c6
PaperChalice <liujunchang97@outlook.com>
2025-02-12 17:40:49 +0800

    update-xliff: update to skip components

--- utils/update-xliff.py
+++ utils/update-xliff.py
@@ -1,12 +1,13 @@
 #! /usr/bin/env python3
 
-from extractor import Extractor, COMPONENT_LIST
+from extractor import Extractor
 import argparse
 from pathlib import Path
 import os
 import time
 import xml.etree.ElementTree as ET
 import warnings
+import langs
 import copy
 
 parser = argparse.ArgumentParser(description='Example argparse script.')
@@ -17,28 +18,20 @@ parser.add_argument('--inreplace', '-i', action='store_true', help='Inreplace mo
 args = parser.parse_args()
 
 class Updater:
-  def __init__(self, component: str):
-    self.component = component
-    input_dir = Path(args.trg_lang.replace('-', '/'))
-
-    file_name = 'xliff'/input_dir/f'Diagnostic{component}Kinds.xlf'
-    self.file_name = file_name
+  def __init__(self, extractor:Extractor, trg_lang:str):
+    self._extractor = extractor
+    self._extract_result = self._extractor.get_result()
+    input_dir = Path(trg_lang.replace('-', '/'))
+    self._file_names = {}
+    self._xlfs = {}
     ET.register_namespace('', 'urn:oasis:names:tc:xliff:document:2.0')
-    self.xlf = ET.parse(file_name)
+    for c in self._extract_result:
+      file_name = 'xliff'/input_dir/f'Diagnostic{c}Kinds.xlf'
+      self._file_names[c] = file_name
+      self._xlfs[c] = ET.parse(file_name)
   
-  def extract(self):
-    extractor = Extractor(self.component)
-    clang_args = ['-std=c++17']
-    if args.clang_prefix:
-      clang_args.append(f'-I{args.clang_prefix/'include'}')
-    if args.llvm_prefix:
-      clang_args.append(f'-I{args.llvm_prefix/'include'}')
-    extractor.extract(clang_args)
-    self.enums = extractor.enums
-  
-  def update(self):
-    self.extract()
-    xlf_copy = copy.deepcopy(self.xlf)
+  def _update_for_component(self, component):
+    xlf_copy = copy.deepcopy(self._xlfs[component])
     ns = {
       'xliff': 'urn:oasis:names:tc:xliff:document:2.0'
     }
@@ -46,7 +39,7 @@ class Updater:
     units = group.findall('./xliff:unit', ns)
     for unit in units:
       group.remove(unit)
-    for e in self.enums:
+    for e in self._extract_result[component]:
       tu = [u for u in units if u.attrib['name'] == e.spelling]
       if len(tu) == 1:
         unit = tu[0]
@@ -66,27 +59,36 @@ class Updater:
         source = ET.SubElement(segment, 'source')
         source.text = e.desc
         ET.SubElement(segment, 'target')
+    file_name = self._file_names[component]
     if not args.inreplace:
-      os.rename(self.file_name, f'{self.file_name}-{int(time.time())}.bak')
-    with open(self.file_name, "wb") as xml_file:
+      os.rename(file_name, f'{file_name}-{int(time.time())}.bak')
+    with open(file_name, "wb") as xml_file:
         ET.indent(xlf_copy)
         xlf_copy.write(xml_file, encoding="UTF-8", xml_declaration=True, short_empty_elements=False)
     return
+  
+  def update(self):
+    for c in self._extract_result:
+      self._update_for_component(c)
       
-
-
-
-def handle_component(component):
-  try:
-    updater = Updater(component)
-    updater.update()
-  except FileNotFoundError:
-    warnings(f'xlf for component "{component}" not found!')
   
 
 def main():
-  for c in COMPONENT_LIST:
-    handle_component(c)
+  clang_args = ['-std=c++17']
+  if args.clang_prefix:
+    clang_args.append(f'-I{args.clang_prefix/'include'}')
+  if args.llvm_prefix:
+    clang_args.append(f'-I{args.llvm_prefix/'include'}')
+  extractor = Extractor()
+  extractor.extract(clang_args)
+  all_langs = [args.trg_lang]
+  if args.trg_lang.upper() == 'ALL':
+    all_langs = langs.ALL_LANGS
+
+  for l in all_langs:
+    updater = Updater(extractor, l)
+    updater.update()
+  
 
 if __name__ == "__main__":
   main()
